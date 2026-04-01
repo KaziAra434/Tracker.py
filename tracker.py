f45a8eb3b070d0b3bf57876f8a6330c550105c47
KaziAra434 <arafatedu31@gmail.com>
2026-04-01 20:56:02 +0600

    Add files via upload

---
+++ tracker.py
@@ -0,0 +1,72 @@
+# "Welcome to Daily Life Tracker Program"
+
+# 3. User information section
+
+"""a = input("Name: ")
+b = float(input("Available hours for today: "))
+c = int(input("Daily budget: "))
+
+print(a)
+print(b)
+print(c)"""
+
+# 4. Daily Activity Input
+
+"""x = input("How many hours you spend on")
+a = float(input("Studying Python: "))
+b = float(input("Practicing Coding: "))
+c = float(input("Others Activities: "))
+
+Total_Activity = (a + b + c)
+
+print(a)
+print(b)
+print(c)
+print("Total Activity:",Total_Activity)"""
+
+# 5. Expense Input
+
+"""a = input("Food expense: ")
+b = input("Transportation expense: ")
+c = input("Other expense: ")
+
+item_a = int(a)
+item_b = int(b)
+item_c = int(c)
+
+Total = (item_a + item_b + item_c)
+
+print("Total daily expense:", Total)"""
+
+# 6. Time Planning Check
+
+"""a = float(input("Total planned hours: "))
+b = float(input("Available hours: "))
+if a > b:
+    print("You have planned more hours than available.")
+else: 
+    print("Your daily plan is realstic.")"""
+
+# 7. Budget Check
+
+"""print("Compare: Total Expense with Daily Budget")
+a = int(input("Your daily expense: "))
+b = int(input("Your Daily Budget: "))
+if a > b:
+    print("You have exceeded your daily budget.")
+else: 
+    print("You are within your daily budget.")"""
+
+# 8. Final Summary Output
+
+print("Final Report")
+
+name = ("Alex!")
+Total_Planned_hours = 6
+Total_Expense = 1500
+Remaining_Budget = 500
+
+print("Hello",name, "This is your final report.")
+print("Total Planned Hours:",Total_Planned_hours)
+print("Total Expense:",Total_Expense)
+print("Remaining Budget:",Remaining_Budget)
